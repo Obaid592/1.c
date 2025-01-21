8b6af6e998669ebac53b617ca492ac46e80f1646
Obaid592 <hashimsiddiqui183@gmail.com>
2025-01-21 10:04:40 +0530

    Add files via upload

---
+++ exp1.1.c
@@ -0,0 +1,31 @@
+
+
+/*
+Exp NO: 1.1
+Title: WAP to print your name, age, class, division
+           and UIN on the screen
+Name :: Siddiqui Obaidullah Abdul Qayyum
+UIN: 241P025
+Div: C
+*/
+#include <stdio.h>
+int main() {
+    printf("\t\t\tSTUDENT INFORMATION\n\n\n");
+    printf("Name\t: Siddiqui Obaidullah Abdul Qayyum\n");
+    printf("Age\t: 18\n");
+    printf("Class\t: FE comp\n");
+    printf("Div\t: C\n");
+    printf("UIN\t: 241P025");
+
+}
+return 0;
+/*OUTPUT
+STUDENT INFORMATION
+Name: Siddiqui Obaidullah Abdul Qayyum
+Age    :18
+Class : FE comp
+Div  : C
+UIN :241P025
+*/
+
+    
