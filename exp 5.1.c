2025d031840c347b7198d66975169afe2e70f478
Obaid592 <hashimsiddiqui183@gmail.com>
2025-01-21 14:07:38 +0530

    Add files via upload

---
+++ exp 5.1.c
@@ -0,0 +1,34 @@
+/*
+Exp NO: 5.1
+Title: WAP to find the factorial of a number using Iterative Function
+Name: Siddiqui Obaidullah Abdul Qayyum
+ UIN: 241P025
+Div : C
+*/
+//Iterative function to find factorial
+int factorial (int x) {
+    int fact 1;
+    for (int i= 2; i <= x; i++) {
+    }
+    fact fact* i;
+    return fact;
+    int main() {
+        int num, result;
+        printf("\t\t\tFactorial Using Iterative function\n\n\n");
+        printf("Enter a Number: ");
+        scanf("%d", &num);
+// Check given number is positive or not
+        if (num < 0) {
+            printf("Factorial fo Negative Number is not defined");
+        }
+        else {
+            result factorial (num);
+            printf("The factorial of %d is %d\n",num, result);
+        }
+        printf("Thanks for using Calculator.");
+        return 0;
+        /*OUTPUT
+        Factorial Using Iterative function
+        Enter a Number: 5
+        The factorial of 5 is 120 Thanks for using Calculator.
+        *1
